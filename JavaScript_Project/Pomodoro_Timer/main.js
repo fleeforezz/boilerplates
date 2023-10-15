b8c08fcf9abf151226eb4fc64f09ce11dada27d6
Trương Minh Nhật <fleeforezz@gmail.com>
2023-10-15 22:03:51 +0700

    Update

---
+++ JavaScript_Project/Pomodoro_Timer/main.js
@@ -0,0 +1,76 @@
+let focusButton = document.getElementById("focus");
+let buttons = document.querySelectorAll(".btn");
+let shortBreakButton = document.getElementById("longbreak");
+let startBtn = document.getElementById("btn-start");
+let reset = document.getElementById("btn-reset");
+let pause = document.getElementById("btn-pause");
+let time = document.getElementById("time");
+let set;
+let count = 59;
+let paused = true;
+let minCount = 24;
+time.textContent = `${minCount + 1}:00`;
+
+const appendZero = (value) => {
+  // value = value < 10 ? `0${value}` : value;
+  if (value < 10) {
+    return `0${value}`;
+  }
+  else {
+    return value;
+  }
+  return value;
+};
+
+reset.addEventListener(
+  "click",
+  (resetTime = () => {
+    pauseTimer();
+    count = 59;
+    minCount = 24;
+    time.textContent = `{minCount + 1}:00`;
+  })
+);
+
+const removeFocus = () => {
+  buttons.forEach((btn) => {
+    btn.classList.remove("btn-focus");
+  });
+}
+
+focusButton.addEventListener("click", () => {
+  removeFocus();
+  focusButton.classList.add("btn-focus");
+  pauseTimer();
+  minCount = 24;
+  count = 59;
+  time.textContent = `${minCount + 1}:00`;
+});
+
+shortBreakButton.addEventListener("click", () => {
+  removeFocus();
+  shortBreakButton.classList.add("btn-focus");
+  pauseTimer();
+  minCount = 4;
+  count = 59;
+  time.textContent = `${appendZero(minCount + 1)}:00`;
+});
+
+longBreakButton.addEventListener("click", () => {
+  removeFocus();
+  longBreakButton.classList.add("btn-focus");
+  pauseTimer();
+  minCount = 14;
+  count = 59;
+  time.textContent = `${minCount + 1}:00`;
+});
+
+pause.addEventListener(
+  "click",
+  (pauseTime = () => {
+    paused = true;
+    clearInterval(set);
+    startBtn.classList.remove("show");
+    reset.classList.remove("show");
+  })
+);
