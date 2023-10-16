161ced6348ec230e4d551a3b56ba284c25cceb15
Trương Minh Nhật <fleeforezz@gmail.com>
2023-10-16 10:09:12 +0700

    Update main.js

--- JavaScript_Project/Pomodoro_Timer/main.js
+++ JavaScript_Project/Pomodoro_Timer/main.js
@@ -1,24 +1,121 @@
+// let focusButton = document.getElementById("focus");
+// let buttons = document.querySelectorAll(".btn");
+// let shortBreakButton = document.getElementById("longbreak");
+// let startBtn = document.getElementById("btn-start");
+// let reset = document.getElementById("btn-reset");
+// let pause = document.getElementById("btn-pause");
+// let time = document.getElementById("time");
+// let set;
+// let count = 59;
+// let paused = true;
+// let minCount = 24;
+// time.textContent = `${minCount + 1}:00`;
+
+// const appendZero = (value) => {
+//   // value = value < 10 ? `0${value}` : value;
+//   if (value < 10) {
+//     return `0${value}`;
+//   }
+//   else {
+//     return value;
+//   }
+//   return value;
+// };
+
+// reset.addEventListener(
+//   "click",
+//   (resetTime = () => {
+//     pauseTimer();
+//     count = 59;
+//     minCount = 24;
+//     time.textContent = `{minCount + 1}:00`;
+//   })
+// );
+
+// const removeFocus = () => {
+//   buttons.forEach((btn) => {
+//     btn.classList.remove("btn-focus");
+//   });
+// }
+
+// focusButton.addEventListener("click", () => {
+//   removeFocus();
+//   focusButton.classList.add("btn-focus");
+//   pauseTimer();
+//   minCount = 24;
+//   count = 59;
+//   time.textContent = `${minCount + 1}:00`;
+// });
+
+// shortBreakButton.addEventListener("click", () => {
+//   removeFocus();
+//   shortBreakButton.classList.add("btn-focus");
+//   pauseTimer();
+//   minCount = 4;
+//   count = 59;
+//   time.textContent = `${appendZero(minCount + 1)}:00`;
+// });
+
+// longBreakButton.addEventListener("click", () => {
+//   removeFocus();
+//   longBreakButton.classList.add("btn-focus");
+//   pauseTimer();
+//   minCount = 14;
+//   count = 59;
+//   time.textContent = `${minCount + 1}:00`;
+// });
+
+// pause.addEventListener(
+//   "click",
+//   (pauseTime = () => {
+//     paused = true;
+//     clearInterval(set);
+//     startBtn.classList.remove("hide");
+//     reset.classList.remove("show");
+//     reset.classList.remove("show");
+//   })
+// );
+
+// startBtn.addEventListener("click", () => {
+//   reset.classList.add("show");
+//   paused.classList.add("show");
+//   startBtn.classList.add("hide");
+//   startBtn.classList.remove("show");
+//   if (paused) {
+//     paused = false;
+//     time.textContent = `${appendZero (minCount)}:${appendZero(count)}`;
+//     set = setInterval(() => {
+//       count--;
+//       time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+//       if (count == 0) {
+//         if (minCount != 0) {
+//           minCount--;
+//           count = 60;
+//         } else {
+//           clearInterval(set);
+//         }
+//       }
+//     }, 1000);
+//   }
+// });
+
 let focusButton = document.getElementById("focus");
 let buttons = document.querySelectorAll(".btn");
-let shortBreakButton = document.getElementById("longbreak");
+let shortBreakButton = document.getElementById("shortbreak");
+let longBreakButton = document.getElementById("longbreak");
 let startBtn = document.getElementById("btn-start");
 let reset = document.getElementById("btn-reset");
 let pause = document.getElementById("btn-pause");
 let time = document.getElementById("time");
 let set;
+let active = "focus";
 let count = 59;
 let paused = true;
 let minCount = 24;
 time.textContent = `${minCount + 1}:00`;
 
 const appendZero = (value) => {
-  // value = value < 10 ? `0${value}` : value;
-  if (value < 10) {
-    return `0${value}`;
-  }
-  else {
-    return value;
-  }
+  value = value < 10 ? `0${value}` : value;
   return value;
 };
 
@@ -26,9 +123,19 @@ reset.addEventListener(
   "click",
   (resetTime = () => {
     pauseTimer();
+    switch (active) {
+      case "long":
+        minCount = 14;
+        break;
+      case "short":
+        minCount = 4;
+        break;
+      default:
+        minCount = 24;
+        break;
+    }
     count = 59;
-    minCount = 24;
-    time.textContent = `{minCount + 1}:00`;
+    time.textContent = `${minCount + 1}:00`;
   })
 );
 
@@ -36,7 +143,7 @@ const removeFocus = () => {
   buttons.forEach((btn) => {
     btn.classList.remove("btn-focus");
   });
-}
+};
 
 focusButton.addEventListener("click", () => {
   removeFocus();
@@ -48,6 +155,7 @@ focusButton.addEventListener("click", () => {
 });
 
 shortBreakButton.addEventListener("click", () => {
+  active = "short";
   removeFocus();
   shortBreakButton.classList.add("btn-focus");
   pauseTimer();
@@ -57,6 +165,7 @@ shortBreakButton.addEventListener("click", () => {
 });
 
 longBreakButton.addEventListener("click", () => {
+  active = "long";
   removeFocus();
   longBreakButton.classList.add("btn-focus");
   pauseTimer();
@@ -67,10 +176,34 @@ longBreakButton.addEventListener("click", () => {
 
 pause.addEventListener(
   "click",
-  (pauseTime = () => {
+  (pauseTimer = () => {
     paused = true;
     clearInterval(set);
-    startBtn.classList.remove("show");
+    startBtn.classList.remove("hide");
+    pause.classList.remove("show");
     reset.classList.remove("show");
   })
 );
+
+startBtn.addEventListener("click", () => {
+  reset.classList.add("show");
+  pause.classList.add("show");
+  startBtn.classList.add("hide");
+  startBtn.classList.remove("show");
+  if (paused) {
+    paused = false;
+    time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+    set = setInterval(() => {
+      count--;
+      time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+      if (count == 0) {
+        if (minCount != 0) {
+          minCount--;
+          count = 60;
+        } else {
+          clearInterval(set);
+        }
+      }
+    }, 1000);
+  }
+});
