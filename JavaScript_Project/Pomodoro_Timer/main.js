cebd2abfe40c0586c8472e2ec3e8650f07bcacc1
Trương Minh Nhật <fleeforezz@gmail.com>
2023-10-23 23:18:35 +0700

    Update music player

--- JavaScript_Project/Pomodoro_Timer/main.js
+++ JavaScript_Project/Pomodoro_Timer/main.js
@@ -1,3 +1,4 @@
+// Time Function
 let focusButton = document.getElementById("focus");
 let buttons = document.querySelectorAll(".btn");
 let shortBreakButton = document.getElementById("shortbreak");
@@ -106,3 +107,71 @@ startBtn.addEventListener("click", () => {
     }, 1000);
   }
 });
+
+// Media Function
+const typingSound = new Audio('./music/typingSound.mp3');
+const cherryBlossom = new Audio('./music/cherryBlossom.mp3');
+
+// Selecting elements
+const prevBtn = document.querySelector('.previous');
+const playBtn = document.querySelector('.play-pause');
+const nextBtn = document.querySelector('.next');
+const songName = document.querySelector('.song-name');
+const playPauseIcon = document.querySelector('#play-pause-icon');
+
+const songs = [
+  { ele: typingSound, audioName: 'ASMR Typing Sound' },
+  { ele: cherryBlossom, audioName: 'Cherry Blossom Lofi' },
+];
+
+for (const song of songs) {
+  song.ele.addEventListener('ended', () => {
+    updateSong('next');
+    playPauseSong();
+  })
+}
+
+let current = 0;
+let currentSong = songs[current].ele;
+songName.textContent = songs[current].audioName;
+
+prevBtn.addEventListener('click', () => {
+  updateSong('prev');
+  playPauseSong();
+});
+
+playBtn.addEventListener('click', () => {
+  playPauseSong();
+});
+
+nextBtn.addEventListener('click', () => {
+  updateSong('next');
+  playPauseSong();
+});
+
+const updateSong = (action) => {
+  currentSong.pause();
+  currentSong.currentTime = 0;
+
+  if (action === 'next') {
+    current++;
+    if (current > songs.length -1) current = 0;
+  }
+  if (action === 'prev') {
+    current--;
+    if (current < 0) current = songs.length - 1;
+  }
+  currentSong = songs[current].ele;
+  songName.textContent = songs[current].audioName;
+}
+
+const playPauseSong = () => {
+  if (currentSong.paused) {
+    currentSong.play();
+    playPauseIcon.className = 'ph ph-pause';
+  }
+  else {
+    currentSong.pause();
+    playPauseIcon.className = 'ph ph-play';
+  }
+}
